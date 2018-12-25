eb5bb7bbc58b0ae2a280b76fe7992a4e49301394
Kaibin Ye <804983105@qq.com>
2018-12-25 15:11:54 -0500

    plot1 done

--- plot1.R
+++ plot1.R
@@ -11,3 +11,13 @@ DateTime <- strptime(paste(Data$Date,Data$Time),"%d/%m/%Y %H:%M:%S")
 Data2 <- Data[,names(Data)!="Date" & names(Data)!="Time"]
 Data3 <- data.frame(DateTime,Data2)
 
+## plot 1: histogram
+png(filename = "plot1.png", width = 480, height = 480, units = "px", bg="transparent")
+ # Note bg="transparent" set the background color as transparent
+
+hist(Data3$Global_active_power,
+     col = "red",
+     xlab = "Global Active Power (kilowatts)",
+     main = "Global Active Power")
+
+dev.off()
